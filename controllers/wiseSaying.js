0cba2c767cf719035df0a320af28d1c1529a1f72
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-05-11 12:35:17 +0900

    명언 리소스(wiseSaying) controller 에서 json 데이터 로그 찍어보기

--- controllers/wiseSaying.js
+++ controllers/wiseSaying.js
@@ -1,6 +1,7 @@
 // 명언 컨트롤러
 // 내장모듈
 import { wiseSayingModel } from "../models/wiseSaying.js";
+import { INTERNAL_SERVER_ERROR, NOT_FOUND, OK } from "../customModule/statusCode.js";
 /*
  * 1. 랜덤으로 명언 정보 가져오는 컨트롤러
  *
@@ -12,4 +13,14 @@ import { wiseSayingModel } from "../models/wiseSaying.js";
 export async function wiseSayingController(req, res) {
   // 백엔드 서버로부터 요청에 대한 응답받아오는 model 실행결과
   const modelResult = await wiseSayingModel(req.ip);
+  // 모델 실행 결과값에 따라 분기처리
+  // sequelize query 메서드 실패
+  if (modelResult.state === "fail_sequelize") return res.status(INTERNAL_SERVER_ERROR).json(modelResult);
+  // 명언 리소스 찾는 것을 실패했을 때
+  if (modelResult.state === "not_exist") return res.status(NOT_FOUND).json(modelResult);
+  // 성공적으로 명언 정보 가져왔을 때
+  console.log("##### 명언정보 ######".rainbow);
+  console.log(modelResult.wiseSayingContent);
+  console.log(modelResult.celebrity);
+  return res.status(OK).json(modelResult);
 }
