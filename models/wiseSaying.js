ff063926448518de688dfce280c18742be067968
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-05-11 12:37:14 +0900

    wiseSaying model 에서 fetch 메서드를 통해서 데이터가져오고 json 응답값 추출

--- models/wiseSaying.js
+++ models/wiseSaying.js
@@ -1,3 +1,11 @@
 // 명언 모델
+// 내장모듈
+import fetch from "node-fetch";
+import { BACKEND_URL } from "../customModule/constant.js";
+
 // 50가지의 명언 정보 중 랜덤하게 주기
-export async function wiseSayingModel(ip) {}
+export async function wiseSayingModel(ip) {
+  const backendResponse = await fetch(BACKEND_URL + "/wise-saying");
+  const jsonData = await backendResponse.json();
+  return jsonData;
+}
