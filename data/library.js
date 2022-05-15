906bd32652ad6e52a98b2728a2ad820d83975c71
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-05-15 23:17:54 +0900

    library 리소스 fetch 해온 데이터 로그 찍는 주석 삭제

--- data/library.js
+++ data/library.js
@@ -18,26 +18,6 @@ async function getEntireLibraryData() {
     return { state: FAIL_FETCH };
   }
 }
-
-/*
-console.log("##### 전체도서관 로그 찍기 ######");
-// 도서관 정보가 안왔을 때
-if (entireLibraryData.state !== undefined) console.log(entireLibraryData.state);
-else {
-  // 전체 도서관 정보
-  console.log("##### 성공시 첫번째 도서관 정보 ######");
-  console.log(entireLibraryData[0].libraryName);
-  console.log(entireLibraryData[0].libraryType);
-  console.log(entireLibraryData[0].districts);
-  console.log(entireLibraryData[0].address);
-  console.log(entireLibraryData[0].closeDay);
-  console.log(entireLibraryData[0].weekdayOperateTime);
-  console.log(entireLibraryData[0].saturdayOperateTime);
-  console.log(entireLibraryData[0].holidayOperateTime);
-  console.log(entireLibraryData[0].libraryContact);
-  console.log(entireLibraryData[0].averageGrade);
-   */
-
 // 2. 입력한 지역에 따라 도서관 정보 불러오기
 async function getLocalLibraryData(nameOfCity, districts) {
   try {
@@ -56,25 +36,6 @@ async function getLocalLibraryData(nameOfCity, districts) {
     return { state: FAIL_FETCH };
   }
 }
-
-/*
-if (localLibraryData.state !== undefined) {
-  console.log(localLibraryData.state);
-} else {
-  // 도서관 정보가 있을 때 도서관 정보 응답
-  console.log("##### 지역 도서관 검색 : 첫번째 도서관 정보 ######");
-  console.log(localLibraryData[0].libraryName);
-  console.log(localLibraryData[0].libraryType);
-  console.log(localLibraryData[0].districts);
-  console.log(localLibraryData[0].address);
-  console.log(localLibraryData[0].closeDay);
-  console.log(localLibraryData[0].weekdayOperateTime);
-  console.log(localLibraryData[0].saturdayOperateTime);
-  console.log(localLibraryData[0].holidayOperateTime);
-  console.log(localLibraryData[0].libraryContact);
-  console.log(localLibraryData[0].averageGrade);
-  }
- */
 // 3. 특정 도서관 정보 가져오기
 async function getDetailLibraryData(libraryIndex) {
   try {
@@ -91,20 +52,3 @@ async function getDetailLibraryData(libraryIndex) {
     return { state: FAIL_FETCH };
   }
 }
-/*
-if (localLibraryData.state !== undefined) console.log(localLibraryData.state);
-else {
-  console.log("##### 특정인덱스의 도서관 정보 ######");
-  console.log(localLibraryData.libraryName);
-  console.log(localLibraryData.libraryType);
-  console.log(localLibraryData.districts);
-  console.log(localLibraryData.address);
-  console.log(localLibraryData.closeDay);
-  console.log(localLibraryData.weekdayOperateTime);
-  console.log(localLibraryData.saturdayOperateTime);
-  console.log(localLibraryData.holidayOperateTime);
-  console.log(localLibraryData.libraryContact);
-  console.log(localLibraryData.countOfGrade);
-  console.log(localLibraryData.averageGrade);
-}
- */
