bae39bc686fad431e1763a2f8355c5ab9fd41bab
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-06-08 23:00:08 +0900

    https 적용 및 sitemap.xml 적용

--- app.js
+++ app.js
@@ -16,12 +16,17 @@ import "dotenv/config.js";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
+import http from "http";
+import https from "https";
+import * as fs from "fs";
+
+// 사이트맵 생성
+
 // 내장모듈
 // 날짜/시간 관련 모듈
 import { moment } from "./customs/dateTime.js";
 // js 파일 빌드
 import { build } from "./customs/buildJs.js";
-
 // 라우터
 import boardRouter from "./routes/board.js";
 import commentRouter from "./routes/comment.js";
@@ -65,13 +70,21 @@ if (process.env.NODE_ENV === "development:build") {
 }
 
 // 서버 시작
-if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "development") {
-  app.listen(process.env.PORT, () => {
+if (process.env.NODE_ENV === "development") {
+  http.createServer(app).listen(process.env.PORT, () => {
     console.log(("Start Frontend Server at" + moment().format(" YYYY-MM-DD HH:mm:ss")).rainbow.bold);
   });
 }
-
-
+if (process.env.NODE_ENV === "production") {
+  const options = {
+    key: fs.readFileSync("/etc/letsencrypt/live/mystudylibrary.pe.kr/privkey.pem", "utf-8"),
+    cert: fs.readFileSync("/etc/letsencrypt/live/mystudylibrary.pe.kr/cert.pem", "utf-8"),
+    ca: fs.readFileSync("/etc/letsencrypt/live/mystudylibrary.pe.kr/chain.pem", "utf-8"),
+  };
+  https.createServer(options, app).listen(process.env.PORT, () => {
+    console.log(("Start HTTPS Frontend Server" + moment().format(" YYYY-MM-DD HH:mm:ss")).rainbow.bold);
+  });
+}
 
 // 정적 파일 경로
 app.use("/views", express.static(path.join(__dirname, "views")));
@@ -105,4 +118,8 @@ app.get("/authorized", (req, res) => {
 app.use("/robots.txt", function (req, res) {
   res.type("text/plain");
   res.send("User-agent: *\nAllow: /");
+});
+// sitemap 추가
+app.get("/sitemap.xml", (req, res) => {
+  res.sendFile(__dirname + "/sitemap.xml");
 });
