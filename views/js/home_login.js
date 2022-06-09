cafc209d88f62660e6c0e93ec94681285605a7e9
Zoe0631 <98700133+Zoe0631@users.noreply.github.com>
2022-06-09 16:17:34 +0900

    홈페이지 가짜데이터 없애고 동적으로 데이터 추가되도록하기

--- views/js/home_login.js
+++ views/js/home_login.js
@@ -1,4 +1,3 @@
-
 // 로그인 체크 필요(최초 한번 체크)
 async function checkLogin() {
   const result = await getUserInfo();
@@ -18,33 +17,47 @@ async function logout() {
       location.href = link;
     }
   } else {
-    const result = await sweetAlert(
-      ERROR,
-      "로그아웃 실패",
-      "다시 시도해주세요",
-      `서버 메시지: ${backendResult.state}`
-    );
+    const result = await sweetAlert(ERROR, "로그아웃 실패", "다시 시도해주세요", `서버 메시지: ${backendResult.state}`);
   }
 }
 
 // 데이터 가져오기(최초 1회)
-async function page() {
+async function home() {
   // 최근 게시판 정보
   const boardResult = await getRecentBoard();
-  // 최근 게시판 정보 파싱
-  for (let num in boardResult) {
-    document.getElementsByClassName("home__board--title")[num].innerHTML = `${boardResult[num].postTitle}`;
-    document.getElementsByClassName("home__board--nickname")[
-      num
-      ].innerHTML = `작성자: ${boardResult[num].nickname}`;
-    document.getElementsByClassName("home__board--link")[num].href=`/board/${boardResult[num].boardIndex}`;
+  // 게시물 리스트 생성 & 최근 게시판 정보 파싱
+  if (boardResult.state === undefined) {
+    for (let num in boardResult) {
+      // 게시물 리스트 생성
+      const listElement = document.createElement("li");
+      listElement.classList.add("home__board--list");
+      document.getElementsByClassName("home__board--container")[0].appendChild(listElement);
+      // 하이퍼링크
+      const hrefElement = document.createElement("a");
+      hrefElement.classList.add("home__board--link");
+      hrefElement.setAttribute("href", `/board/${boardResult[num].boardIndex}`);
+      document.getElementsByClassName("home__board--list")[num].appendChild(hrefElement);
+      // 게시글 제목
+      const titleElement = document.createElement("p");
+      titleElement.classList.add("home__board--title");
+      titleElement.textContent = `${boardResult[num].postTitle}`;
+      // 게시글 작성자
+      const nicknameElement = document.createElement("p");
+      nicknameElement.classList.add("home__board--nickname");
+      nicknameElement.textContent = `작성자: ${boardResult[num].nickname}`;
+
+      document.getElementsByClassName("home__board--link")[num].append(titleElement);
+      document.getElementsByClassName("home__board--link")[num].append(nicknameElement);
+    }
   }
   // 명언 정보
   const wiseSayingResult = await getWiseSaying();
-  // 명언 정보 파싱
-  document.getElementsByClassName("home__wiseSaying--content")[0].innerHTML = wiseSayingResult.wiseSayingContent;
-  document.getElementsByClassName("home__wiseSaying--celebrity")[0].innerHTML = wiseSayingResult.celebrity;
 
+  if (boardResult.state === undefined) {
+    // 명언 정보 파싱
+    document.getElementsByClassName("home__wiseSaying--content")[0].innerHTML = wiseSayingResult.wiseSayingContent;
+    document.getElementsByClassName("home__wiseSaying--celebrity")[0].innerHTML = wiseSayingResult.celebrity;
+  }
   // 유저 정보
   const userResult = await getUserInfo();
   document.getElementsByClassName("home__user--nickname")[0].innerHTML = `${userResult.nickname}님`;
@@ -59,6 +72,6 @@ async function page() {
 
 async function lifeCycle() {
   await checkLogin();
-  await page();
+  await home();
 }
 lifeCycle();
--- views/js/home_not_login.js
+++ views/js/home_not_login.js
@@ -11,20 +11,40 @@ async function checkLogin() {
 async function home() {
   // 최근 게시판 정보
   const boardResult = await getRecentBoard();
-  // 최근 게시판 정보 파싱
-  for (let num in boardResult) {
-    document.getElementsByClassName("home__board--title")[num].innerHTML = `${boardResult[num].postTitle}`;
-    document.getElementsByClassName("home__board--nickname")[num].innerHTML = `작성자: ${boardResult[num].nickname}`;
+  // 게시물 리스트 생성 & 최근 게시판 정보 파싱
+  if(boardResult.state===undefined) {
+    for (let num in boardResult) {
+      // 게시물 리스트 생성
+      const listElement = document.createElement("li");
+      listElement.classList.add("home__board--list");
+      document.getElementsByClassName("home__board--container")[0].appendChild(listElement);
+      // 하이퍼링크
+      const hrefElement = document.createElement("a");
+      hrefElement.classList.add("home__board--link");
+      hrefElement.setAttribute("href", `/board/${boardResult[num].boardIndex}`);
+      document.getElementsByClassName("home__board--list")[num].appendChild(hrefElement);
+      // 게시글 제목
+      const titleElement = document.createElement("p");
+      titleElement.classList.add("home__board--title");
+      titleElement.textContent = `${boardResult[num].postTitle}`;
+      // 게시글 작성자
+      const nicknameElement = document.createElement("p");
+      nicknameElement.classList.add("home__board--nickname");
+      nicknameElement.textContent = `작성자: ${boardResult[num].nickname}`;
 
-    document.getElementsByClassName("home__board--link")[num].href = `/board/${boardResult[num].boardIndex}`;
+      document.getElementsByClassName("home__board--link")[num].append(titleElement);
+      document.getElementsByClassName("home__board--link")[num].append(nicknameElement);
+    }
   }
   // 명언 정보
   const wiseSayingResult = await getWiseSaying();
-  // 명언 정보 파싱
-  document.getElementsByClassName("home__wiseSaying--content")[0].innerHTML = wiseSayingResult.wiseSayingContent;
-  document.getElementsByClassName("home__wiseSaying--celebrity")[0].innerHTML = wiseSayingResult.celebrity;
+  if(wiseSayingResult.state===undefined) {
+    // 명언 정보 파싱
+    document.getElementsByClassName("home__wiseSaying--content")[0].innerHTML = wiseSayingResult.wiseSayingContent;
+    document.getElementsByClassName("home__wiseSaying--celebrity")[0].innerHTML = wiseSayingResult.celebrity;
+  }
 }
-async function lifeCycle(){
+async function lifeCycle() {
   await checkLogin();
   await home();
 }
